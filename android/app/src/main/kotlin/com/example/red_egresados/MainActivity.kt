3323dad239388902eb393301e481d27365381fce
ingenieraLesly <88113682+ingenieraLesly@users.noreply.github.com>
2021-11-23 15:41:19 -0500

    Initialize s11-firebase-firestore

---
+++ android/app/src/main/kotlin/com/example/red_egresados/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.red_egresados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/pages/authentication/signup/singup_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:red_egresados/domain/use_cases/controllers/authentication.dart';
+import 'package:red_egresados/domain/use_cases/controllers/connectivity.dart';
+
+class SignUpScreen extends StatefulWidget {
+  final VoidCallback onViewSwitch;
+
+  const SignUpScreen({Key? key, required this.onViewSwitch}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<SignUpScreen> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final controller = Get.find<AuthController>();
+  final connectivityController = Get.find<ConnectivityController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Creación de usuario",
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              key: const Key("signUpName"),
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Usuario',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              key: const Key("signUpEmail"),
+              controller: emailController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Correo electrónico',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              key: const Key("signUpPassword"),
+              controller: passwordController,
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Clave',
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (connectivityController.connected) {
+                        await controller.manager.signUp(
+                            name: nameController.text,
+                            email: emailController.text,
+                            password: passwordController.text);
+                      } else {
+                        Get.showSnackbar(
+                          GetBar(
+                            message: "No estas conectado a la red.",
+                            duration: const Duration(seconds: 2),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text("Registrar"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          TextButton(
+            onPressed: widget.onViewSwitch,
+            child: const Text("Entrar"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
