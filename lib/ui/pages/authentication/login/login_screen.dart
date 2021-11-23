53405fb27ad9827f6b025d264b9195c9d97f1c54
ingenieraLesly <88113682+ingenieraLesly@users.noreply.github.com>
2021-11-23 15:41:19 -0500

    Initialize s13-shared-preferences

---
+++ lib/ui/pages/authentication/login/login_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:get/get.dart';
+import 'package:red_egresados/domain/use_cases/controllers/authentication.dart';
+import 'package:red_egresados/domain/use_cases/controllers/connectivity.dart';
+
+class LoginScreen extends StatefulWidget {
+  final VoidCallback onViewSwitch;
+
+  const LoginScreen({Key? key, required this.onViewSwitch}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final controller = Get.find<AuthController>();
+  final connectivityController = Get.find<ConnectivityController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Iniciar sesión",
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              key: const Key("signInEmail"),
+              controller: emailController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Correo electrónico',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              key: const Key("signInPassword"),
+              controller: passwordController,
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Clave',
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: ElevatedButton(
+                    child: const Text("Login"),
+                    onPressed: () async {
+                      if (connectivityController.connected) {
+                        await controller.manager.signIn(
+                            email: emailController.text,
+                            password: passwordController.text);
+                      } else {
+                        Get.showSnackbar(
+                          GetBar(
+                            message: "No estas conectado a la red.",
+                            duration: const Duration(seconds: 2),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+          TextButton(
+            key: const Key("toSignUpButton"),
+            child: const Text("Registrarse"),
+            onPressed: widget.onViewSwitch,
+          ),
+          SignInButton(
+            Buttons.GoogleDark,
+            onPressed: () => controller.manager.signInWithGoogle(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
